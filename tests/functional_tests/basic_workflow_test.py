e3374cc26c2147c8ad2d1af7acb00318db067fff
Alejandro Esquivel <ae@alejandro.ltd>
2022-12-06 08:59:58 -0800

    removed executor fixture and using aliases

--- tests/functional_tests/basic_workflow_test.py
+++ tests/functional_tests/basic_workflow_test.py
@@ -21,12 +21,10 @@
 import covalent as ct
 import pytest
 
-from tests.functional_tests.fixtures.executor import executor
-
 
 @pytest.mark.functional_tests
 def test_basic_workflow():
-    @ct.electron(executor=executor)
+    @ct.electron(executor="awsbatch")
     def join_words(a, b):
         return ", ".join([a, b])
 
